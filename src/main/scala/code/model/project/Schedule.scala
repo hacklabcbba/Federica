f20653a80a09c1f80b7a0de7ddf5f4f387af490d
Nataly Huanca <nataly.huanca@genso.com.bo>
2015-01-28 09:58:46 -0400

    design model for Project

---
+++ src/main/scala/code/model/project/Schedule.scala
@@ -0,0 +1,21 @@
+package code
+package model
+package project
+
+import code.lib.RogueMetaRecord
+import net.liftweb.mongodb.record.MongoRecord
+import net.liftweb.mongodb.record.field.{ObjectIdRefField, DateField, ObjectIdPk}
+import net.liftweb.record.field.StringField
+
+class Schedule private() extends MongoRecord[Schedule] with ObjectIdPk[Schedule]{
+
+  override def meta = Schedule
+
+  object startDate extends DateField(this)
+  object endDate extends DateField(this)
+  object description extends StringField(this, 500)
+  object city extends ObjectIdRefField(this, City)
+  object country extends ObjectIdRefField(this, Country)
+}
+
+object Schedule extends Schedule with RogueMetaRecord[Schedule]
