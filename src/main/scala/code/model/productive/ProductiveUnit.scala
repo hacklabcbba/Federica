c09296e094372e92c89d1984d4f07b71bfb5105c
Nataly Huanca <nataly.huanca@genso.com.bo>
2015-03-04 15:39:58 -0400

    add fields

--- src/main/scala/code/model/productive/ProductiveUnit.scala
+++ src/main/scala/code/model/productive/ProductiveUnit.scala
@@ -3,6 +3,7 @@ package model
 package productive
 
 import code.lib.RogueMetaRecord
+import code.model.proposal.Area
 import net.liftweb.mongodb.record.MongoRecord
 import net.liftweb.mongodb.record.field.{ObjectIdRefField, ObjectIdPk}
 import net.liftweb.record.field.{BooleanField, StringField}
@@ -14,6 +15,12 @@ class ProductiveUnit private () extends MongoRecord[ProductiveUnit] with ObjectI
   object name extends StringField(this, 500)
   object description extends StringField(this, 500)
   object administrator extends ObjectIdRefField(this, User)
+  object area extends ObjectIdRefField(this, Area) {
+    override def optional_? = true
+  }
+  // when merge object program extends ObjectIdRefField(this, Program){
+  //override def optional_? = true
+  //}
 }
 
 object ProductiveUnit extends ProductiveUnit with RogueMetaRecord[ProductiveUnit]
