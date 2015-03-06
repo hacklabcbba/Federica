bd8999f5e85f3c20bd54626ac98fb8dedc49f443
Nataly Huanca <nataly.huanca@genso.com.bo>
2015-03-06 10:45:16 -0400

    changes

--- src/main/scala/code/model/proposal/Proposal.scala
+++ src/main/scala/code/model/proposal/Proposal.scala
@@ -3,10 +3,12 @@ package model
 package proposal
 
 import code.lib.RogueMetaRecord
+import code.model.activity.ActivityType
 import code.model.project.{Country, City}
-import net.liftweb.mongodb.record.field.{ObjectIdPk, ObjectIdRefField}
+import code.model.resource.Resource
+import net.liftweb.mongodb.record.field.{MongoListField, ObjectIdPk, ObjectIdRefField}
 import net.liftweb.mongodb.record.MongoRecord
-import net.liftweb.record.field.StringField
+import net.liftweb.record.field.{DateTimeField, BooleanField, StringField}
 
 class Proposal private () extends MongoRecord[Proposal] with ObjectIdPk[Proposal] {
   override def meta = Proposal
@@ -20,6 +22,19 @@ class Proposal private () extends MongoRecord[Proposal] with ObjectIdPk[Proposal
   object country extends ObjectIdRefField(this, Country)
   object area extends ObjectIdRefField(this, Area)
   //To do when merge object productiveUnit extends ObjectIdRefField(this, ProductiveUnit)
+  object isAproved extends BooleanField(this, false)
+  object currentDate extends DateTimeField(this)
+  object activityType extends ObjectIdRefField(this, ActivityType)
+  object administrator extends ObjectIdRefField(this, User)
+  object applicant extends ObjectIdRefField(this, User)
+  object dateInit extends DateTimeField(this)
+  object dateEnd extends DateTimeField(this)
+  object resources extends MongoListField[Proposal, Resource[_]](this)
 }
 
 object Proposal extends Proposal with RogueMetaRecord[Proposal]
+
+object ReserveType extends Enumeration {
+  type ReserveType = Value
+  val Continuous, Discontinuous, Loose = Value
+}
--- src/test/scala/code/model/proposal/ProposalSpec.scala
+++ src/test/scala/code/model/proposal/ProposalSpec.scala
@@ -2,6 +2,7 @@ package code
 package model
 package proposal
 
+import code.model.activity.ActivityType
 import code.model.project._
 
 class ProposalSpec extends BaseMongoSessionWordSpec {
@@ -9,45 +10,10 @@ class ProposalSpec extends BaseMongoSessionWordSpec {
   "Proposal" should {
     "create, validate, save, and retrieve properly" in {
 
-      val city = City.createRecord
-        .name("Cochabamba")
-
-      val errsCity = city.validate
-      if (errsCity.length > 1) {
-        fail("Validation error: " + errsCity.mkString(", "))
-      }
-
-      city.validate.length should equal (0)
-
-      city.save(false)
-
-      val country = Country.createRecord
-        .name("Bolivia")
-
-      val errsCountry = country.validate
-      if (errsCountry.length > 1) {
-        fail("Validation error: " + errsCountry.mkString(", "))
-      }
-
-      country.validate.length should equal (0)
-
-      country.save(false)
-
-      val area = Area.createRecord
-        .name(" Visual crafts")
-        .description("Martadero Areas")
-        .email("visual@gmail.com")
-        .isWorkshop(true)
-        .code("ARV")
-
-      val errsArea = area.validate
-      if (errsArea.length > 1) {
-        fail("Validation error: " + errsArea.mkString(", "))
-      }
-
-      area.validate.length should equal (0)
-
-      area.save(false)
+      val city = createCity
+      val country = createCountry
+      val area = createArea
+      val activity = createActivityType
 
       val proposal = Proposal.createRecord
         .description("Include information about recent international progress in the field of the research, and the " +
@@ -60,6 +26,8 @@ class ProposalSpec extends BaseMongoSessionWordSpec {
         "interested in economic history, the history of economic thought, and political and cultural histories of economic life. ")
         .name("Big History Project")
         .state("In review")
+        .isAproved(false)
+        .activityType(activity.id.get)
         .city(city.id.get)
         .country(country.id.get)
         .area(area.id.get)
@@ -74,5 +42,66 @@ class ProposalSpec extends BaseMongoSessionWordSpec {
       proposal.save(false)
 
     }
+  }
+
+  def createCity = {
+    val city = City
+      .createRecord
+      .name("Cochabamba")
+
+    val errsCity = city.validate
+    if (errsCity.length > 1) {
+      fail("Validation error: " + errsCity.mkString(", "))
+    }
+
+    city.validate.length should equal (0)
+    city.save(false)
+    city
+  }
+
+  def createCountry = {
+    val country = Country
+      .createRecord
+      .name("Bolivia")
+
+    val errsCountry = country.validate
+    if (errsCountry.length > 1) {
+      fail("Validation error: " + errsCountry.mkString(", "))
+    }
+
+    country.validate.length should equal (0)
+    country.save(false)
+  }
+
+  def createActivityType = {
+    val activityType = ActivityType
+      .createRecord
+      .name("Proyection")
+
+    val errsActivity = activityType.validate
+    if (errsActivity.length > 1) {
+      fail("Validation error: " + errsActivity.mkString(", "))
+    }
+
+    activityType.validate.length should equal (0)
+    activityType.save(false)
+  }
+
+  def createArea = {
+    val area = Area
+      .createRecord
+      .name(" Visual crafts")
+      .description("Martadero Areas")
+      .email("visual@gmail.com")
+      .isWorkshop(true)
+      .code("ARV")
+
+    val errsArea = area.validate
+    if (errsArea.length > 1) {
+      fail("Validation error: " + errsArea.mkString(", "))
+    }
+
+    area.validate.length should equal (0)
+    area.save(false)
   }
 }
