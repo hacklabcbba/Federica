142ab511c7838b89df7e26f6c2bbcf564c7b5466
Juan Jose Olivera <juan@genso.com.bo>
2016-01-22 10:12:54 -0400

    fix #86

--- src/main/scala/code/model/Process.scala
+++ src/main/scala/code/model/Process.scala
@@ -47,33 +47,35 @@ class Process private () extends MongoRecord[Process] with ObjectIdPk[Process] w
   }
 
   object area extends ObjectIdRefField(this, Area) {
-    override def displayName = "Área"
     override def optional_? = true
-    override def toString = obj.dmap("")(_.name.get)
-    val listAreas = Area.findAll
-    override def toForm = {
-      Full(SHtml.selectElem(
-        listAreas,
-        obj,
-        "class" -> "select2 form-control",
-        "data-placeholder" -> "Seleccione area.."
-      )(a => set(a.id.get)))
+    override def displayName = "Área"
+    override def toString = {
+      this.obj.dmap("Indefinido..")(_.name.get)
     }
+    override def toForm = {
+      Full(SHtml.selectObj[Option[Area]](availableOptions, Full(this.obj),
+        (p: Option[Area]) => {
+          setBox(p.map(_.id.get))
+        },
+        "class" -> "select2 form-control",
+        "data-placeholder" -> "Seleccione area transversal.."))
+    }
+
+    def availableOptions = (None -> "Ninguna") :: Area.findAll.map(s => Some(s) -> s.toString)
   }
 
   object transversalArea extends ObjectIdRefField(this, TransversalArea) {
-
+    override def optional_? = true
     override def displayName = "Área transversal"
     override def toString = this.obj.dmap("")(_.name.get)
-    val list = TransversalArea.findAll
-    val default = list.headOption
+    val list = (None -> "Ninguna") :: TransversalArea.findAll.map(s => Some(s) -> s.toString)
     override def toForm = {
-      Full(SHtml.ajaxSelectElem(list, default,
+      Full(SHtml.selectObj[Option[TransversalArea]](list, Full(this.obj),
+        (p: Option[TransversalArea]) => {
+          setBox(p.map(_.id.get))
+        },
         "class" -> "select2 form-control",
-        "data-placeholder" -> "Seleccione area transversal..")(p => {
-        set(p.id.get)
-        Noop
-      }))
+        "data-placeholder" -> "Seleccione area transversal.."))
     }
   }
 
@@ -81,14 +83,14 @@ class Process private () extends MongoRecord[Process] with ObjectIdPk[Process] w
     override def displayName = "Programa"
     override def optional_? = true
     override def toString = obj.dmap("")(_.name.get)
-    val listProgram = Program.findAll
+    val list = (None -> "Ninguno") :: Program.findAll.map(s => Some(s) -> s.toString)
     override def toForm = {
-      Full(SHtml.selectElem(
-        listProgram,
-        obj,
+      Full(SHtml.selectObj[Option[Program]](list, Full(this.obj),
+        (p: Option[Program]) => {
+          setBox(p.map(_.id.get))
+        },
         "class" -> "select2 form-control",
-        "data-placeholder" -> "Seleccione program.."
-      )(s => set(s.id.get)))
+        "data-placeholder" -> "Seleccione prorgrama.."))
     }
   }
 
