6099d9465202e1d672afc7ee22abc12f8cabab79
Juan Jose Olivera <juan@genso.com.bo>
2016-01-28 11:48:47 -0400

    fix #82

--- src/main/scala/code/model/resource/Room.scala
+++ src/main/scala/code/model/resource/Room.scala
@@ -4,8 +4,14 @@ package resource
 
 import code.config.Site
 import code.lib.RogueMetaRecord
-import code.lib.field.{BsStringField, FileField}
-import net.liftweb.record.field.BooleanField
+import code.lib.field.{BsDoubleField, BsStringField, FileField}
+import net.liftweb.common.{Full, Box}
+import net.liftweb.mongodb.record.field.BsonRecordListField
+import net.liftweb.mongodb.record.{BsonMetaRecord, BsonRecord}
+import net.liftweb.record.LifecycleCallbacks
+import net.liftweb.record.field.{EnumField, BooleanField}
+
+import scala.xml.NodeSeq
 
 class Room private() extends Resource[Room] {
 
@@ -59,6 +65,258 @@ class Room private() extends Resource[Room] {
     }
   }
 
+  object areasCost extends BsonRecordListField(this, Cost) with LifecycleCallbacks {
+    override def displayName = "Costos por area"
+    private var newCosts: List[Cost] = Nil
+    override def toForm: Box[NodeSeq] = Full {
+      Area.findAll.foldLeft(NodeSeq.Empty){ case (node, area) => {
+        val cost = this.value.find(cost => cost.area.get == area.id.get).getOrElse{
+          val c = Cost.createRecord.area(area.id.get)
+          newCosts = newCosts ++ List(c)
+          c
+        }
+        node ++
+          <div class="row">
+            <div class="col-md-6"><h5>{area.name.get}</h5></div>
+            <div class="col-md-3">C.S.</div>
+            <div class="col-md-3">S.S.</div>
+          </div> ++
+          <div class="row">
+            <div class="form-group margin-bottom-xs">
+              <label data-name="label" class="control-label col-md-6" for="email_id">Privado</label>
+              <div class="col-md-3">
+                {cost.costCSPrivate.toForm openOr NodeSeq.Empty}
+                <span class="input-group-addon">
+                  Bs.
+                </span>
+              </div>
+              <div class="col-md-3">
+                {cost.costSSPrivate.toForm openOr NodeSeq.Empty}
+                <span class="input-group-addon">
+                  Bs.
+                </span>
+              </div>
+            </div>
+          </div> ++
+          <div class="row">
+            <div class="form-group margin-bottom-xs">
+              <label data-name="label" class="control-label col-md-6" for="email_id">Coordinador</label>
+              <div class="col-md-3">
+                {cost.costCSCoord.toForm openOr NodeSeq.Empty}
+                <span class="input-group-addon">
+                  Bs.
+                </span>
+              </div>
+              <div class="col-md-3">
+                {cost.costSSCoord.toForm openOr NodeSeq.Empty}
+                <span class="input-group-addon">
+                  Bs.
+                </span>
+              </div>
+            </div>
+          </div> ++
+          <div class="row">
+            <div class="form-group margin-bottom-xs">
+              <label data-name="label" class="control-label col-md-6" for="email_id">Asociado</label>
+              <div class="col-md-3">
+                {cost.costCSAssociate.toForm openOr NodeSeq.Empty}
+                <span class="input-group-addon">
+                  Bs.
+                </span>
+              </div>
+              <div class="col-md-3">
+                {cost.costSSAssociate.toForm openOr NodeSeq.Empty}
+                <span class="input-group-addon">
+                  Bs.
+                </span>
+              </div>
+            </div>
+          </div> ++
+          <div class="row">
+            <div class="form-group margin-bottom-xs">
+              <label data-name="label" class="control-label col-md-6" for="email_id">Artista</label>
+              <div class="col-md-3">
+                {cost.costCSArtist.toForm openOr NodeSeq.Empty}
+                <span class="input-group-addon">
+                  Bs.
+                </span>
+              </div>
+              <div class="col-md-3">
+                {cost.costSSArtist.toForm openOr NodeSeq.Empty}
+                <span class="input-group-addon">
+                  Bs.
+                </span>
+              </div>
+            </div>
+          </div> ++
+          <div class="row">
+            <div class="form-group margin-bottom-xs">
+              <label data-name="label" class="control-label col-md-6" for="email_id">Institución cultural con fines de lucro</label>
+              <div class="col-md-3">
+                {cost.costCSInst1.toForm openOr NodeSeq.Empty}
+                <span class="input-group-addon">
+                  Bs.
+                </span>
+              </div>
+              <div class="col-md-3">
+                {cost.costSSInst1.toForm openOr NodeSeq.Empty}
+                <span class="input-group-addon">
+                  Bs.
+                </span>
+              </div>
+            </div>
+          </div> ++
+          <div class="row">
+            <div class="form-group margin-bottom-xs">
+              <label data-name="label" class="control-label col-md-6" for="email_id">Institución cultural sin fines de lucro</label>
+              <div class="col-md-3">
+                {cost.costCSInst2.toForm openOr NodeSeq.Empty}
+                <span class="input-group-addon">
+                  Bs.
+                </span>
+              </div>
+              <div class="col-md-3">
+                {cost.costSSInst2.toForm openOr NodeSeq.Empty}
+                <span class="input-group-addon">
+                  Bs.
+                </span>
+              </div>
+            </div>
+          </div>
+      }}
+    }
+
+    override def beforeSave: Unit = {
+      this.set(this.get ++ newCosts)
+    }
+  }
+
+  object programsCost extends BsonRecordListField(this, Cost) with LifecycleCallbacks {
+    override def displayName = "Costos por programa"
+    private var newCosts: List[Cost] = Nil
+    override def toForm: Box[NodeSeq] = Full {
+      Program.findAll.foldLeft(NodeSeq.Empty){ case (node, program) => {
+        val cost = this.value.find(cost => cost.program.get == program.id.get).getOrElse{
+          val c = Cost.createRecord.program(program.id.get)
+          newCosts = newCosts ++ List(c)
+          c
+        }
+        node ++
+          <div class="row">
+            <div class="col-md-6"><h5>{program.name.get}</h5></div>
+            <div class="col-md-3">C.S.</div>
+            <div class="col-md-3">S.S.</div>
+          </div> ++
+          <div class="row">
+            <div class="form-group margin-bottom-xs">
+              <label data-name="label" class="control-label col-md-6" for="email_id">Privado</label>
+              <div class="col-md-3">
+                {cost.costCSPrivate.toForm openOr NodeSeq.Empty}
+                <span class="input-group-addon">
+                  Bs.
+                </span>
+              </div>
+              <div class="col-md-3">
+                {cost.costSSPrivate.toForm openOr NodeSeq.Empty}
+                <span class="input-group-addon">
+                  Bs.
+                </span>
+              </div>
+            </div>
+          </div> ++
+          <div class="row">
+            <div class="form-group margin-bottom-xs">
+              <label data-name="label" class="control-label col-md-6" for="email_id">Coordinador</label>
+              <div class="col-md-3">
+                {cost.costCSCoord.toForm openOr NodeSeq.Empty}
+                <span class="input-group-addon">
+                  Bs.
+                </span>
+              </div>
+              <div class="col-md-3">
+                {cost.costSSCoord.toForm openOr NodeSeq.Empty}
+                <span class="input-group-addon">
+                  Bs.
+                </span>
+              </div>
+            </div>
+          </div> ++
+          <div class="row">
+            <div class="form-group margin-bottom-xs">
+              <label data-name="label" class="control-label col-md-6" for="email_id">Asociado</label>
+              <div class="col-md-3">
+                {cost.costCSAssociate.toForm openOr NodeSeq.Empty}
+                <span class="input-group-addon">
+                  Bs.
+                </span>
+              </div>
+              <div class="col-md-3">
+                {cost.costSSAssociate.toForm openOr NodeSeq.Empty}
+                <span class="input-group-addon">
+                  Bs.
+                </span>
+              </div>
+            </div>
+          </div> ++
+          <div class="row">
+            <div class="form-group margin-bottom-xs">
+              <label data-name="label" class="control-label col-md-6" for="email_id">Artista</label>
+              <div class="col-md-3">
+                {cost.costCSArtist.toForm openOr NodeSeq.Empty}
+                <span class="input-group-addon">
+                  Bs.
+                </span>
+              </div>
+              <div class="col-md-3">
+                {cost.costSSArtist.toForm openOr NodeSeq.Empty}
+                <span class="input-group-addon">
+                  Bs.
+                </span>
+              </div>
+            </div>
+          </div> ++
+          <div class="row">
+            <div class="form-group margin-bottom-xs">
+              <label data-name="label" class="control-label col-md-6" for="email_id">Institución cultural con fines de lucro</label>
+              <div class="col-md-3">
+                {cost.costCSInst1.toForm openOr NodeSeq.Empty}
+                <span class="input-group-addon">
+                  Bs.
+                </span>
+              </div>
+              <div class="col-md-3">
+                {cost.costSSInst1.toForm openOr NodeSeq.Empty}
+                <span class="input-group-addon">
+                  Bs.
+                </span>
+              </div>
+            </div>
+          </div> ++
+          <div class="row">
+            <div class="form-group margin-bottom-xs">
+              <label data-name="label" class="control-label col-md-6" for="email_id">Institución cultural sin fines de lucro</label>
+              <div class="col-md-3">
+                {cost.costCSInst2.toForm openOr NodeSeq.Empty}
+                <span class="input-group-addon">
+                  Bs.
+                </span>
+              </div>
+              <div class="col-md-3">
+                {cost.costSSInst2.toForm openOr NodeSeq.Empty}
+                <span class="input-group-addon">
+                  Bs.
+                </span>
+              </div>
+            </div>
+          </div>
+      }}
+    }
+
+    override def beforeSave: Unit = {
+      this.set(this.get ++ newCosts)
+    }
+  }
+
 }
 
 object Room extends Room with RogueMetaRecord[Room] {
@@ -77,3 +335,4 @@ object Room extends Room with RogueMetaRecord[Room] {
     Room.where(_.classType eqs ClassType.RoomType).fetch()
   }
 }
+
