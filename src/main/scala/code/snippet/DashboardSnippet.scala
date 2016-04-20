60bcb1e37aaa8f96272247731dbbbd0f2d53b8c2
Nataly Nanda <natalynanda@gmail.com>
2016-04-20 18:17:04 -0400

    add dashboard main page

---
+++ src/main/scala/code/snippet/DashboardSnippet.scala
@@ -0,0 +1,29 @@
+package code.snippet
+
+import code.config.Site
+import code.model.User
+import code.model.event.Event
+import net.liftmodules.extras.SnippetHelper
+import net.liftweb.http.js.JsCmds
+import net.liftweb.http.{S, SHtml}
+import net.liftweb.http.js.JsCmds.{RedirectTo, Run}
+import net.liftweb.util.CssSel
+import net.liftweb.util.Helpers._
+
+object DashboardSnippet extends SnippetHelper {
+
+  def render: CssSel = {
+    for {
+      user <- User.currentUser ?~ "For see this section do you have to login"
+    } yield {
+      "data-name=checkbox" #> SHtml.ajaxCheckbox(false, a => {
+        S.appendJs(Run(
+          """
+            var sendbtn = document.getElementById('createEvent');
+            sendbtn.disabled = """ + (!a).toString + """;
+          """.stripMargin))
+      }) &
+      "data-name=createEvent [onclick]" #> SHtml.ajaxInvoke(() => RedirectTo(Site.backendEventAdd.calcHref(Event.createRecord)))
+    }
+  }
+}
